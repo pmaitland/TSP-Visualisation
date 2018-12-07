87b02b01286917a34db10262d253e0935a4ecb27
Peter Maitland <pmaitland98@gmail.com>
2018-12-07 16:16:24 +0000

    Updated canvas.js

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -13,6 +13,7 @@ var vertexStrokeWeight = 2,
 var vertexColour         = "#fff",
     selectedVertexColour = "#fff",
     vertexBorderColour   = "#000",
+    vertexLabelColour    = "#000",
 
     edgeColour       = "#000",
     edgeWeightColour = "#000",
@@ -24,10 +25,13 @@ var vertexColour         = "#fff",
     nearestVertexColour    = "#7bd389",
     partOfTourVertexColour = "#778da9",
     partOfTreeVertexColour = "#ff7c1d",
+    oddDegreeVertexColour  = "#e817b4",
+    //"#e84747",
 
-    neartestEdgeColour   = "#7bd389",
-    partOfTourEdgeColour = "#778da9",
-    partOfTreeEdgeColour = "#ff7c1d";
+    neartestEdgeColour       = "#7bd389",
+    partOfTourEdgeColour     = "#778da9",
+    partOfTreeEdgeColour     = "#ff7c1d",
+    partofMatchingEdgeColour = "#e817b4";
 
 /**
   Called once at the very beginning.
@@ -59,9 +63,11 @@ function draw() {
     drawDistanceMarkings();
 
   drawAnimationEdges();
-  drawCurvedEdges();
   drawEdges();
   drawVertices();
+  drawAnimationEdgeWeights();
+  if (!inEuclideanSpace)
+    drawNonEuclideanVertexLabels();
   drawEdgeWeights();
 
   if (mouseX > 0 && mouseX < canvasWidth && mouseY > 0 && mouseY < windowHeight) {
@@ -81,10 +87,14 @@ function updateCanvasLayout() {
 				angle = (vertex.id / (vertexCount / 2)) * Math.PI,
 				x = (r * Math.cos(angle)) + (canvasWidth / 2),
 				y = (r * Math.sin(angle)) + (windowHeight / 2),
+        nonEuclideanLabelX = ((r + 20) * Math.cos(angle)) + (canvasWidth / 2),
+				nonEuclideanLabelY = ((r + 20) * Math.sin(angle)) + (windowHeight / 2),
         radius = 15;
 
     vertex.x = x;
     vertex.y = y;
+    vertex.nonEuclideanLabelX = nonEuclideanLabelX;
+    vertex.nonEuclideanLabelY = nonEuclideanLabelY;
     vertex.radius = radius;
 	}
 }
@@ -141,20 +151,7 @@ function drawEdgeWeights() {
 function drawAnimationEdges() {
   var v1, v2;
 
-  for (let edge of edgesToNearest) {
-    v1 = edge[0];
-    v2 = edge[1];
-
-    stroke(neartestEdgeColour);
-    strokeWeight(animatedEdgeStrokeWeight);
-    line(v1.x, v1.y, v2.x, v2.y);
-
-    fill(edgeWeightColour);
-    strokeWeight(1);
-    textSize(16);
-    text(distances[v1.id][v2.id], (v1.x + v2.x) / 2, (v1.y + v2.y) /2);
-  }
-
+  // tree edges
   for (let edge of edgesInTree) {
     v1 = edge[0];
     v2 = edge[1];
@@ -164,6 +161,56 @@ function drawAnimationEdges() {
     line(v1.x, v1.y, v2.x, v2.y);
   }
 
+  // matching edges
+  let dashLength = 4;
+  for (let edge of edgesInMatching) {
+    v1 = edge[0];
+    v2 = edge[1];
+
+    let leftVertex;
+    if (v1.x < v2.x) leftVertex = v1;
+    else leftVertex = v2;
+
+    let topVertex;
+    if (v1.y < v2.y) topVertex = v1;
+    else topVertex = v2;
+
+    let distanceBetweenVertices = distanceBetween(v1, v2);
+    let xDistance = Math.abs(v1.x - v2.x);
+    let yDistance = Math.abs(v1.y - v2.y);
+    let numberOfDashes = distanceBetweenVertices / (dashLength * 2);
+    let dx = xDistance / numberOfDashes;
+    let dy = yDistance / numberOfDashes;
+
+    let xValues = [];
+    if (leftVertex.id == v1.id) {
+      for (let i = 0; i < numberOfDashes; i++) {
+        xValues.push(leftVertex.x + i*dx);
+      }
+    } else {
+      for (let i = numberOfDashes - 1; i >= 0; i--) {
+        xValues.push(leftVertex.x + i*dx);
+      }
+    }
+
+    let yValues = [];
+    if (topVertex.id == v1.id) {
+      for (let i = 0; i < numberOfDashes; i++) {
+        yValues.push(topVertex.y + i*dy);
+      }
+    } else {
+      for (let i = numberOfDashes - 1; i >= 0; i--) {
+        yValues.push(topVertex.y + i*dy);
+      }
+    }
+
+    stroke(partofMatchingEdgeColour);
+    strokeWeight(animatedEdgeStrokeWeight);
+    for (let i = 0; i < numberOfDashes; i+=2)
+      line(xValues[i], yValues[i], xValues[i+1], yValues[i+1]);
+  }
+
+  // tour edges
   for (let edge of edgesInTour) {
     v1 = edge[0];
     v2 = edge[1];
@@ -172,31 +219,28 @@ function drawAnimationEdges() {
     strokeWeight(animatedEdgeStrokeWeight);
     line(v1.x, v1.y, v2.x, v2.y);
   }
+
+  // nearest neighbour edges
+  for (let edge of edgesToNearest) {
+    v1 = edge[0];
+    v2 = edge[1];
+
+    stroke(neartestEdgeColour);
+    strokeWeight(animatedEdgeStrokeWeight);
+    line(v1.x, v1.y, v2.x, v2.y);
+  }
 }
 
-function drawCurvedEdges() {
-  for (let i = 0; i < curvedEdges.length; i++) {
+function drawAnimationEdgeWeights() {
+  // nearest neighbour edge weights
+  for (let edge of edgesToNearest) {
+    v1 = edge[0];
+    v2 = edge[1];
 
-    let edge = curvedEdges[i];
-
-    let v1 = edge[0],
-        v2 = edge[1];
-
-    let c = {x: v1.x, y: v2.y};
-
-    let x1 = c.x,
-        y1 = c.y,
-        x2 = v1.x,
-        y2 = v1.y,
-        x3 = v2.x,
-        y3 = v2.y,
-        x4 = c.x,
-        y4 = c.y;
-
-    noFill();
-    stroke(edgeColour);
-    strokeWeight(animatedEdgeStrokeWeight);
-    curve(x1, y1, x2, y2, x3, y3, x4, y4);
+    fill(edgeWeightColour);
+    strokeWeight(1);
+    textSize(16);
+    text(distances[v1.id][v2.id], (v1.x + v2.x) / 2, (v1.y + v2.y) /2);
   }
 }
 
@@ -217,6 +261,8 @@ function drawVertices() {
       fill(partOfTreeVertexColour);
     if (vertex.isNearest)
       fill(nearestVertexColour);
+    if (vertex.isOddDegree)
+      fill(oddDegreeVertexColour);
     if (vertex.isPartOfTour)
       fill(partOfTourVertexColour);
     if (vertex.isAt)
@@ -224,6 +270,22 @@ function drawVertices() {
 
     stroke(vertexBorderColour);
     ellipse(vertex.x, vertex.y, vertex.radius);
+  }
+}
+
+function drawVertexLabel(v) {
+  fill(vertexLabelColour);
+  strokeWeight(1);
+  textSize(16);
+  text(v.label, v.x - v.radius / 2, v.y - v.radius);
+}
+
+function drawNonEuclideanVertexLabels() {
+  for (let v of vertices) {
+    fill(vertexLabelColour);
+    strokeWeight(1);
+    textSize(16);
+    text(v.label, v.nonEuclideanLabelX - 5, v.nonEuclideanLabelY + 5);
   }
 }
 
@@ -264,8 +326,8 @@ function mousePressed() {
       let distance = distanceBetween(v, vertices[i]);
       distances[i][id] = distance;
       distances[id][i] = distance;
-      distances[id][id] = 0;
     }
+    distances[id][id] = 0;
     vertexCount++;
 
     displayDistanceMatrix();
